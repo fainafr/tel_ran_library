60827d484363690de39a384465c3fb00e578d11f
pvitkovsky <vitkovsky1337@gmail.com>
2018-09-04 20:53:36 +0300

    Partitions demo

---
+++ src/test/CollectorsPartitionDemo.java
@@ -0,0 +1,31 @@
+package test;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import dao.Book;
+
+public class CollectorsPartitionDemo {
+	public static void main(String[] args){
+		partitioningByDemo();
+		partitioningByCountDemo();
+	}
+
+	/**
+	 * Demostrates partitioningBy method in collectors; 
+	 */
+	private static void partitioningByDemo() {
+		Stream.generate(Book::getRandomBook)
+		.limit(30)
+		.collect(Collectors.partitioningBy((Book b) -> b.getPrice() < 500.))
+		.entrySet()
+		.forEach(System.out::println);
+	}
+	private static void partitioningByCountDemo() {
+		Stream.generate(Book::getRandomBook)
+		.limit(30)
+		.collect(Collectors.partitioningBy((Book b) -> b.getPrice() < 500., Collectors.counting()))
+		.entrySet()
+		.forEach(System.out::println);
+	}
+}
